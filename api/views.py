08fe883b4798e60e694328be647f4b4b32e6f60d
IGICOCORO <nduwimanaguymich@gmail.com>
2022-02-04 23:08:58 +0200

    changed type field

--- api/views.py
+++ api/views.py
@@ -1,10 +1,9 @@
-from typing import List
-
 from rest_framework import viewsets, mixins
 from rest_framework.decorators import action
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
+from django.db import transaction
 
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -21,6 +20,20 @@ class ComptePrincipalViewset(viewsets.ModelViewSet):
 	authentication_classes = [JWTAuthentication, SessionAuthentication]
 	permission_classes = [IsAuthenticated]
 
+	def create(self,request):
+		serializer = self.get_serializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		data = request.data
+		comptes = ComptePrincipal.objects.all()
+		if comptes:
+			compte:ComptePrincipal = comptes.first()
+			compte.montant += int(data["montant"])
+			compte.save()
+			serializer = ComptePrincipalSerializer(compte, many=False)
+		else:
+			serializer.save()
+		return Response(serializer.data,201)
+
 
 class ParcelleViewset(viewsets.ModelViewSet):
 	serializer_class = ParcelleSerializer
@@ -34,11 +47,36 @@ class MotoViewset(viewsets.ModelViewSet):
 	authentication_classes = [JWTAuthentication, SessionAuthentication]
 	permission_classes = [IsAuthenticated]
 
+	@transaction.atomic
+	def create(self, request, *args, **kwargs):
+		serializer = self.get_serializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		data = request.data
+		compte:ComptePrincipal = ComptePrincipal.objects.first()
+		compte.montant -= int(data["prix_achat_unitaire"])
+		compte.montant -= int(data["autres_depenses"])
+		compte.save()
+		serializer.save()
+		return Response(serializer.data,201)
+
+
 class CreditViewset(viewsets.ModelViewSet):
 	serializer_class = CreditSerializer
 	queryset = Credit.objects.all()
 	authentication_classes = [JWTAuthentication, SessionAuthentication]
 	permission_classes = [IsAuthenticated]
+
+
+	@transaction.atomic
+	def create(self, request, *args, **kwargs):
+		serializer = self.get_serializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		data = request.data
+		credit:ComptePrincipal = ComptePrincipal.objects.first()
+		credit.montant -= int(data["montant"])
+		credit.save()
+		serializer.save()
+		return Response(serializer.data,201)
 
 class AutresInvestissementViewset(viewsets.ModelViewSet):
 	serializer_class = AutresInvestissementSerializer
